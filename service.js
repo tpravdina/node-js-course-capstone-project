44da4e9eb13fd28c8c9832c1cbed88511778eecb
Tetiana Pravdina <tpravdina@griddynamics.com>
2021-12-24 12:07:38 +0200

    add validation

--- service.js
+++ service.js
@@ -1,4 +1,5 @@
 const db = require("./db-handlers");
+const { isIdValid, isLimitValid, isDateValid } = require("./validation");
 
 const showStartPage = (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -42,12 +43,47 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
+const invalidValue404Error = (res, valueName) => {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.write(`Invalid "${valueName}" value.`);
+  res.end();
+};
+
 const getUserLog = async (req, res, next) => {
   const id = req.params._id;
 
   const limit = req.query.limit;
   const from = req.query.from;
   const to = req.query.to;
+
+  // Validation
+  if (!isIdValid(id)) {
+    invalidValue404Error(res, "_id");
+    next();
+    return;
+  }
+
+  if (limit) {
+    if (!isLimitValid(limit)) {
+      invalidValue404Error(res, "limit");
+      next();
+      return;
+    }
+  }
+  if (from) {
+    if (!isDateValid(from)) {
+      invalidValue404Error(res, "from");
+      next();
+      return;
+    }
+  }
+  if (to) {
+    if (!isDateValid(to)) {
+      invalidValue404Error(res, "to");
+      next();
+      return;
+    }
+  }
 
   try {
     const userLog = await db.getUserLog(id, limit, from, to);
